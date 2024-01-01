748f1b00b5df887a33c228e47d16bf06ce2654b2
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-01-01 13:49:42 +0530

    service layer developed with implemenattion

---
+++ pos-backend/src/main/java/lk/ijse/pos/service/CategoryService.java
@@ -0,0 +1,15 @@
+package lk.ijse.pos.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import lk.ijse.pos.entity.CategoryEntity;
+
+@Service
+public interface CategoryService {
+    CategoryEntity createCategory(CategoryEntity categoryEntity);
+    CategoryEntity getCategoryById(Long id);
+    List<CategoryEntity> getAllCategories();
+    CategoryEntity updateCategory(Long id, CategoryEntity categoryEntity);
+}
