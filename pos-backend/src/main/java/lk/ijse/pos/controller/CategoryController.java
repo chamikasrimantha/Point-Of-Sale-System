738f72e2ee88d883413b9b2955e39074bdc5472e
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-01-03 14:01:38 +0530

    delete method added in controllers

--- pos-backend/src/main/java/lk/ijse/pos/controller/CategoryController.java
+++ pos-backend/src/main/java/lk/ijse/pos/controller/CategoryController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -39,6 +40,11 @@ public class CategoryController {
     @PutMapping("/categories/{id}")
     public CategoryEntity updateCategory(@PathVariable Long id, @RequestBody CategoryEntity categoryEntity){
         return categoryService.updateCategory(id, categoryEntity);
+    }
+
+    @DeleteMapping("/categories/{id}")
+    public CategoryEntity deleteCategory(@PathVariable Long id){
+        return categoryService.deleteCategory(id);
     }
 
 }
--- pos-backend/src/main/java/lk/ijse/pos/controller/ItemController.java
+++ pos-backend/src/main/java/lk/ijse/pos/controller/ItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -61,6 +62,11 @@ public class ItemController {
     @GetMapping("/categories/{id}/items")
     public ResponseEntity<List<ItemEntity>> getItemsByCategory(@PathVariable Long id){
         return ResponseEntity.ok().body(itemService.getItemsByCategory(id));
+    }
+
+    @DeleteMapping("/items/{id}")
+    public ItemEntity deleteCategory(@PathVariable Long id){
+        return itemService.deleteItem(id);
     }
 
 }
