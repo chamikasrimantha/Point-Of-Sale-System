1aa4966e2bde5827b806d7e315a529a61569abe0
Chamika Srimantha <chamikasrimantha2002@gmail.com>
2024-01-07 11:08:32 +0530

    changes added

--- pos-frontend/src/adminpages/AdminHome.js
+++ pos-frontend/src/adminpages/AdminHome.js
@@ -1,15 +1,23 @@
 import React, { useEffect } from 'react'
+import './Admin.css'
 import AdminNavBar from './components/AdminNavBar'
 
 export default function AdminHome() {
 
-    useEffect(() => {
+  useEffect(() => {
 
-    }, [])
+  }, [])
 
   return (
     <div>
-      <AdminNavBar/>
+      <AdminNavBar />
+
+      <div className="admin-home">
+        <div className="admin-home-content">
+          <h1>Welcome to Admin Dashboard</h1>
+          <p>POS Software ...</p>
+        </div>
+      </div>
     </div>
   )
 }
